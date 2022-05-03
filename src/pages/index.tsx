1886f110f7c64dc45868e7cf72ef19a9d8e37e8a
quintaro <octavius.sys@gmail.com>
2022-05-03 17:58:53 -0500

    feat:test prisma-client in home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,35 @@
-import React from 'react'
-import {NextPage} from 'next'
-interface Props{
+import React from "react"
+import { GetServerSideProps, NextPage } from "next"
+import { prismaAdmin } from "@/../db"
+import { PollQestion } from "@prisma/client"
+import { Center, Code, Heading, List, ListItem, Text } from "@chakra-ui/react"
 
+interface Props {
+  //qestions:PollQestion[]|null
 }
-const HomePage:NextPage<Props> = ({}) => {
-return (
-<div>HomePage</div>
-)
+const HomePage: NextPage = (props: any) => {
+  const a = JSON.parse(props.questions)
+  console.log(a)
+
+  return (
+    <Center>
+      <Heading>List of Questions</Heading>
+      <Code>{props.questions}</Code>
+      {/* {qestions?<List>
+    {qestions.map(q=>(
+    <ListItem key={q.id}>{q.question}</ListItem>
+    ))}
+  </List>:<Text>Not found</Text>} */}
+    </Center>
+  )
 }
 export default HomePage
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const questions: PollQestion[] = await prismaAdmin.pollQestion.findMany()
+  return {
+    props: {
+      questions: JSON.stringify(questions),
+    },
+  }
+}
