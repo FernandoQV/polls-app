757f8d52a2b23808493cddee8a5be41833923e54
quintaro <octavius.sys@gmail.com>
2022-05-04 01:03:04 -0500

    feat:tRPC is ok

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,6 +1,6 @@
 import * as trpc from '@trpc/server';
 import * as trpcNext from '@trpc/server/adapters/next';
-import { z } from 'zod';
+import { object, z } from 'zod';
 
 export const appRouter = trpc
   .router()
@@ -15,7 +15,13 @@ export const appRouter = trpc
         greeting: `hello ${input?.text ?? 'world'}`,
       };
     },
-  });
+  }).query('getPolls',{
+      input:z.object({h:z.number()}).nullish(),resolve({input}){
+          return {
+              message:`el numero es: ${input?.h ?? 7}`
+          }
+      }
+  })
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,15 @@ import { GetServerSideProps, NextPage } from "next"
 import { prismaAdmin } from "@/../db"
 import { PollQestion } from "@prisma/client"
 import { Center, Code, Heading } from "@chakra-ui/react"
+import { trpc } from "@/utils/trpc"
 
 interface Props {
 questions:any
 }
 const HomePage: NextPage<Props> = ({questions}) => {
   
+  const fde=trpc.useQuery(['getPolls',{h:45}])
+  console.log(fde);
   
   return (
     <Center>
